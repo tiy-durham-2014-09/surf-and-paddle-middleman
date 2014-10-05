a87f7217fb287dcaac544fef2b44d804f0b5a8fa
David Perry <curiousepic@gmail.com>
2014-10-05 10:56:31 -0400

    Index sidebar, Index and stylesheet cleanup

--- config.rb
+++ config.rb
@@ -55,6 +55,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :articles_dir, 'articles'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
